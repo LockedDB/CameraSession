909eb35e3186c6f263b96f87854de65909b8bda9
Daniel BENET <daniel.benet@outlook.es>
2024-04-16 19:27:47 +0200

    Initial Commit

---
+++ CameraSession/CameraSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSessionApp.swift
+//  CameraSession
+//
+//  Created by Dani Benet on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
